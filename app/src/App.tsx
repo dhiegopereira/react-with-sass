b2b76d3438d7319373bdbf7798a44f1eae46d328
Your Name <you@example.com>
2022-10-17 14:17:00 -0300

    first commit

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="title">
+      Adicionando SASS
+    </div>
+  );
+}
+
+export default App;
